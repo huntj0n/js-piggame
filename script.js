b300082b985f51dac6db2a2f0496b14e54883fd7
huntj0n <hunt.jon19@gmail.com>
2023-02-05 20:24:04 -0700

    roll dice functionality. noticing a current player display bug. initial fix push

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -15,12 +16,15 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   //1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+
   //2. Display dice
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
@@ -29,8 +33,14 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     //add dice number to current score
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     current0El.textContent = currentScore; //change later
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    console.log(activePlayer);
   }
 });
