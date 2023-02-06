4601394a2b86fa9bda1da5b723040c7d2ee386fe
huntj0n <hunt.jon19@gmail.com>
2023-02-06 06:03:08 -0700

    hold button functionality

--- script.js
+++ script.js
@@ -22,6 +22,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
 //Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   //1. Generating a random dice roll
@@ -39,10 +47,27 @@ btnRoll.addEventListener("click", function () {
       currentScore;
   } else {
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  //1. add current score to active players score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //2. Check if player's score is already at leastt 100
+  // - if so end the game
+  if (scores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+  } else {
+    // - if not switch player
+    switchPlayer();
   }
 });
